f8c3ee1346615fe216fd14207b6ffeb540c77931
Lissa Walzer <Lissa@Eric-30.local>
2015-11-20 09:58:31 -0800

    Improved the comments on the kernel function

--- js/imageLoop.js
+++ js/imageLoop.js
@@ -87,7 +87,7 @@ $(document).ready(function() {
         //create a new, empty image used to make the edited picture
         var newImage = context.createImageData(canvas.width, canvas.height);
         var newPixels = newImage.data;
-        //various kernels
+        //various kernel matrices 
         var blurKernel = [[0.0625, 0.125, 0.0625], [0.125, 0.25, 0.125], [0.0625, 0.125, 0.0625]];
         var edgeKernel = [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]];
         var sharpenKernel = [[0, -1, 0], [-1, 5, -1], [0, -1, 0]];
